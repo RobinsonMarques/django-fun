97c062a82bdf60d280476470e42af295da557c04
unknown <Python@BUE302D26.alunos.sc.senac.br>
2021-01-20 17:20:34 -0300

    relacional

--- pokemaoc/views.py
+++ pokemaoc/views.py
@@ -5,7 +5,7 @@ import pokebase as pb
 
 # Make a request to api and take some fields to our view
 def simple(request):
-    response = requests.get('https://pokeapi.co/api/v2/pokemon/1')
+    response = requests.get('https://pokeapi.co/api/v2/pokemon/4')
     pokemon = response.json()
     return render(request, 'pokemaoc/simple.html', {
         'nome': pokemon['name'],
@@ -19,8 +19,6 @@ def package(request):
         pokemon_name = request.GET['pokemon_name']
         sprite = {'url': pb.SpriteResource('pokemon', pokemon_name).url}
     return render(request, 'pokemaoc/package.html', {'sprite':sprite})
-
-
 
 # https://dev.to/camerenisonfire/10-intriguing-public-rest-apis-for-your-next-project-2gbd
 # https://pokeapi.co/docs/v2#pokemon
